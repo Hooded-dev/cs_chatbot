a57fd7f3a57fc5f741ea6294e8c28a40dda5d77c
Hooded-dev <abdulabeyg@gmail.com>
2023-07-22 20:26:42 +0500

    commit with base structure

---
+++ app.py
@@ -0,0 +1,47 @@
+import chromadb
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.llms import OpenAI
+from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.document_loaders import DirectoryLoader
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.document_loaders import SeleniumURLLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chat_models import ChatOpenAI
+import os
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import DirectoryLoader
+import os
+import openai
+from langchain.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+import inspect
+
+from getpass import getpass
+from langchain import OpenAI
+from langchain.chains import LLMChain, ConversationChain
+from langchain.chains.conversation.memory import (ConversationBufferMemory,
+                                                  ConversationSummaryMemory,
+                                                  ConversationBufferWindowMemory,
+                                                  ConversationKGMemory)
+from langchain.callbacks import get_openai_callback
+import tiktoken
+import os
